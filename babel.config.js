375481b91c33b08dfea5a751381282872171f403
Drew Powers <drew@pow.rs>
2020-11-25 11:49:37 -0700

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: ['@babel/preset-react'],
+  plugins: [
+    [
+      'import-globals',
+      {
+        React: 'react',
+        Component: { moduleName: 'react', exportName: 'Component' },
+        PropTypes: { moduleName: 'react', exportName: 'PropTypes' },
+      },
+    ],
+  ],
+};
